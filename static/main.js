0d77698639d5955f2d8da0266c5104d4e0079d16
DarkMatter-999 <darkmatter999official@gmail.com>
2022-06-26 19:36:48 +0530

    Added hamburger menu

--- static/main.js
+++ static/main.js
@@ -1,5 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
+const hmenu = document.querySelector('.hmenu');
 
-hamburger.addEventListener('click', function() {
-   this.classList.toggle('is-active');
+hamburger.addEventListener("click", function() {
+   this.classList.toggle("is-active");
+   hmenu.classList.toggle("is-active");
 });
